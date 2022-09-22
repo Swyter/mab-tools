00c55dfc4c2392d99a98eb68de380bf61f1d7f16
Swyter <swyterzone@gmail.com>
2022-09-22 21:28:15 +0200

    sco_repack: Fix the last block of the 'path' layer in scn_lebennin_coast_3 being one element short because (last_zero == -1) didn't apply here. It should be safe to use i >= layer_last_idx if we're at the end.
    Fix inserting the donor AI mesh size being off by the 4 bytes of the first size field itself.

--- mab_sco_repack.py
+++ mab_sco_repack.py
@@ -53,7 +53,7 @@ def write_over_from(output_f, donor, write_mission_objects = False, write_ai_mes
 
         if (write_ai_mesh):
             donor_f.seek(ai_mesh_start_pos, io.SEEK_SET)
-            output_f.write(donor_f.read(ai_mesh_section_size))
+            output_f.write(donor_f.read(ai_mesh_section_size + 4))
 
         if (write_terrain):
             donor_f.seek(terrain_start_pos, io.SEEK_SET)
@@ -426,10 +426,13 @@ def sco_repack(input_folder, output_sco, mission_objects_from = False, ai_mesh_f
                             # swy: fix ground_elevation being off by one when there are no preceding zeroes
                             #      we are at the end of the line, writing the block due to being the last idx
                             #      and we want a slice as big as the entire data array (i + 1)
-                            last_slice_idx = last_zero == -1 and i + 1 or i
+                            last_slice_idx = i >= layer_last_idx and i + 1 or i
 
                             data_slice = layer_data[last_zero + 1: last_slice_idx]
                             amount_of_preceding_zeros = ((last_zero + 1) - (first_zero + 1)) + 1
+
+                            if amount_of_preceding_zeros == 36 and layer_name == 'path':
+                                a=1
 
                             if (amount_of_preceding_zeros < 0):
                                 print("[e] the amount of preceding zeroes can't be negative")
@@ -493,7 +496,7 @@ Quick examples:
     parser.add_argument('-te', '--terrain',        dest='sect_terrain',         default='repack', metavar='<option>', required=False,
                         help='by default the <option> is «repack», it will convert back the unpacked data in the folder you provide. You can use «keep» to retain the original data in the target SCO if it exists and avoid modifying that part, which is also faster than repacking and lossless, you can use «empty» or «blank» to completely remove any data previously that section, or, finally; you can provide a path to a different donor .sco file to copy that section over directly into the target .sco, losslessly replacing a section/block without having to unpack it first or merge it manually.')
 
-    args = parser.parse_args() #'scn_lebennin_coast_3'.split())
+    args = parser.parse_args('C:\\Users\\Usuario\\Documents\\github\\tldmod\\SceneObj\\scn_lebennin_coast_3 -mo C:\\Users\\Usuario\\Documents\\github\\tldmod\\SceneObj\\scn_lebennin_coast_3_orig.sco'.split())
 
 
     if args.sect_mission_objects == args.sect_ai_mesh == args.sect_terrain == 'keep':

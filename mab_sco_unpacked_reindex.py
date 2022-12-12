19a9f41b8da24b255f7679ea23d07f1b9e70f016
Swyter <swyterzone@gmail.com>
2022-12-12 20:34:49 +0100

    sco_unpacked_reindex: turns out there is no magic way of handling both flora_kind tree-flagged entry formats. So we need to use some workarounds based on content hints.

--- mab_sco_unpacked_reindex.py
+++ mab_sco_unpacked_reindex.py
@@ -73,12 +73,22 @@ def sco_unpacked_reindex(input_folder, opt_scene_props_txt = '', opt_remove_miss
                     elem_flag          = int(opt_flora_kinds_txt_lines[cur_line][1])
                     elem_num_of_meshes = int(opt_flora_kinds_txt_lines[cur_line][2]) #
 
-                    # cumular |= elem_flag; hex = ("%013x" % elem_flag); cum = ("%013x" % cumular) # 0x07fe011e0c0c ^ 0x07fe015e0c0c = 0x400000 # swy: append all the previous flags until this point to find which ones are different for trees
-                    # print(f"{hex} {cum} {opt_flora_kinds_txt_lines[cur_line][0]}")
-                    fkf_tree      = 0x00400000
-                    fkf_speedtree = 0x02000000
-                    elem_is_flagged_as_speedtree = (elem_flag & (fkf_tree | fkf_speedtree)) == (fkf_tree | fkf_speedtree)
-                    cur_line += 1 + elem_num_of_meshes * (elem_is_flagged_as_speedtree and 2 or 1) # swy: trees seem to have two lines per mesh variant, others just one line per mesh
+
+                    fkf_tree      = 0x00400000; fkf_speedtree = 0x02000000
+                    elem_is_flagged_as_tree = not not (elem_flag & (fkf_tree | fkf_speedtree))
+
+                    # swy: guess what, warband crashes reading the original M&B 1.011 flora_kinds format because it
+                    #      expects the extra " 0 0" line after each tree mesh and there's no header with version 
+                    #      indication, so to handle both we'll need to look for the two zeros in the first mesh
+                    #      entry, if that's there it has to be generated for Warband, otherwise it's not.
+                    elem_has_wb_format = 0
+                    if elem_is_flagged_as_tree:
+                        if len(opt_flora_kinds_txt_lines[cur_line + 2]) == 2 and \
+                            opt_flora_kinds_txt_lines[cur_line + 2][0] == '0' and \
+                            opt_flora_kinds_txt_lines[cur_line + 2][1] == '0':
+                            elem_has_wb_format = 1
+
+                    cur_line += 1 + elem_num_of_meshes * (elem_is_flagged_as_tree and elem_has_wb_format and 2 or 1) # swy: in warband only, trees seem to have two lines per mesh variant, others just one line per mesh
 
                 print(f'[-] loading {opt_flora_kinds_txt_count:4} total flora kinds from the mod .txt file')
 

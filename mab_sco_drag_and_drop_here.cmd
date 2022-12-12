2d98cb03796bb5ed5fb36a96301714e3b6695f40
Swyter <swyterzone@gmail.com>
2022-12-12 13:58:12 +0100

    sco_drag_and_drop_here: Add a handy batch script one can drop any .sco / folder / mission_objects.json files onto and it will call the right tool and leave the window open to see what the log does, instead of immediately closing.

---
+++ mab_sco_drag_and_drop_here.cmd
@@ -0,0 +1,43 @@
+@echo off && color 71 && cls && title swyscotools log-- && chcp 65001 > nul
+
+setlocal enableextensions
+setlocal enabledelayedexpansion
+set PATH=!PATH!;%CD%
+
+if "%~1"=="" (
+    :show_help
+    MODE CON: COLS=91 LINES=7
+    echo  ┌───────────────────────────────────────────────────────────────────────────────────────┐
+    echo  │ Drag and drop your .sco file, mission_objects.json or unpacked folder over            │
+    echo  │ the icon or shortcut and this will call the right program and let you see the output  │
+    echo  └───────────────────────────────────────────────────────────────────────────────────────┘
+    echo. 
+    pause
+    goto :eof
+)
+
+MODE CON: COLS=180
+if exist "%1/"                   goto :repack
+if %~x1  ==                .sco  goto :unpack
+if %~nx1 == mission_objects.json goto :unpacked_reindex
+
+goto :show_help
+
+:unpack
+    echo.[/] calling «mab_sco_unpack» %* && echo.    --
+    "%~dp0/mab_sco_unpack" %*
+    goto :end
+
+:unpacked_reindex
+    echo.[/] calling mab_sco_unpacked_reindex %~dp1 && echo.    --
+    "%~dp0/mab_sco_unpacked_reindex" %~dp1
+    goto :end
+
+:repack
+    echo.[/] calling mab_sco_repack %* && echo.    --
+    "%~dp0/mab_sco_repack" %*
+    goto :end
+
+:end
+    pause
+    goto :eof

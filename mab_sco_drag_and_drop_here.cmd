e52366333aff0959d8b5654bdbcec4c2ce8eaa7f
Swyter <swyterzone@gmail.com>
2022-12-12 18:31:51 +0100

    sco_drag_and_drop_here: Improve a bit more the output of this handy script.

--- mab_sco_drag_and_drop_here.cmd
+++ mab_sco_drag_and_drop_here.cmd
@@ -8,7 +8,7 @@ if "%~1"=="" (
     :show_help
     MODE CON: COLS=91 LINES=7
     echo  ┌───────────────────────────────────────────────────────────────────────────────────────┐
-    echo  │ Drag and drop your .sco file, mission_objects.json or unpacked folder over            │
+    echo  │ Drag and drop your .sco file, mission_objects.json or unpacked scn_ folder over       │
     echo  │ the icon or shortcut and this will call the right program and let you see the output  │
     echo  └───────────────────────────────────────────────────────────────────────────────────────┘
     echo. 
@@ -24,18 +24,18 @@ if %~nx1 == mission_objects.json goto :unpacked_reindex
 goto :show_help
 
 :unpack
-    echo.[/] calling «mab_sco_unpack» %* && echo.    --
-    "%~dp0/mab_sco_unpack" %*
+    echo.[/] calling «mab_sco_unpack» %~1 && echo.    -- && title sco_unpack %~1
+    "%~dp0/mab_sco_unpack" %~1
     goto :end
 
 :unpacked_reindex
-    echo.[/] calling mab_sco_unpacked_reindex %~dp1 && echo.    --
+    echo.[/] calling «mab_sco_unpacked_reindex» %~dp1 && echo.    -- && title sco_unpacked_reindex %~dp1
     "%~dp0/mab_sco_unpacked_reindex" %~dp1
     goto :end
 
 :repack
-    echo.[/] calling mab_sco_repack %* && echo.    --
-    "%~dp0/mab_sco_repack" %*
+    echo.[/] calling «mab_sco_repack» %~1 && echo.    -- && title sco_repack %~1
+    "%~dp0/mab_sco_repack" %~1
     goto :end
 
 :end

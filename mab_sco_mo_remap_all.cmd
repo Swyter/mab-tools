5b0846eb0b866803fb1910c1cafe059f5f4e1d45
Swyter <swyterzone@gmail.com>
2023-05-11 07:02:08 +0200

    sco_mo_remap: Make the sample script easier to read and understand, add further comments.

--- mab_sco_mo_remap_all.cmd
+++ mab_sco_mo_remap_all.cmd
@@ -13,9 +13,21 @@ for %%f in (scn_*.sco) do (
     echo.
     echo -- processing %%f
 
-    echo. && echo ** 1^) unpack && mab_sco_unpack.exe            "%%f" -o _tmp_unpack_folder  --dont-unpack-aimesh  --dont-unpack-terrain             && rem swy: extract our current filename .sco into an empty _tmp_unpack_folder, only extract the mission_objects.json to save time, we don't need to extract the terrain or AI mesh, we won't touch it, and if we repack it there can be differences.
-    echo. && echo ** 2^) reindx && mab_sco_unpacked_reindex.exe  --dont-reindex  --remappingfile mab_sco_mo_remap.txt  _tmp_unpack_folder             && rem swy: use our very own mab_sco_mo_remap.txt file to search for replacements, use that coupled with the data from ../../scene_props.txt to tweak _tmp_unpack_folder/mission_objects.json and do the replacements. You can add --scenepropstxt scene_props.txt to make it look up that file somewhere else when not in a SceneObj folder, remove --dont-reindex to make it update the IDs/numbers after the rename.
-    echo. && echo ** 3^) repack && mab_sco_repack.exe            _tmp_unpack_folder -o "%%f"  --missionobjects repack  --aimesh keep  --terrain keep  && rem swy: use the extracted _tmp_unpack_folder as input, use the original filename .sco as output. only use/read the mission_objects.json from that extracted folder, leave the other two .sco terrain/AI mesh sections alone/pristine from the original, just copy them over.
+    :: swy: extract our current filename .sco into an empty _tmp_unpack_folder, only extract the mission_objects.json to save time, we don't need to
+    ::      extract the terrain or AI mesh, we won't touch it, and if we repack it there can be differences.
+    echo. && echo ** 1^) unpack && mab_sco_unpack.exe            "%%f" -o _tmp_unpack_folder  --dont-unpack-aimesh  --dont-unpack-terrain
+
+    :: swy: use our very own mab_sco_mo_remap.txt file to search for replacements, use that coupled with the data from ../../scene_props.txt to tweak
+    ::      _tmp_unpack_folder/mission_objects.json and do the replacements. You can add --scenepropstxt scene_props.txt to make it look up that
+    ::      file somewhere else when we're not in a SceneObj folder. Remove --dont-reindex to make it update the IDs/numbers after the rename.
+    echo. && echo ** 2^) reindx && mab_sco_unpacked_reindex.exe  --dont-reindex  --remappingfile mab_sco_mo_remap.txt  _tmp_unpack_folder
+
+    :: swy: use the extracted _tmp_unpack_folder as input, use the original filename .sco as output. only use/read the mission_objects.json from that
+    :       extracted folder, leave the other two .sco terrain/AI mesh sections alone/pristine from the original, just copy them over.
+    echo. && echo ** 3^) repack && mab_sco_repack.exe            _tmp_unpack_folder -o "%%f"  --missionobjects repack  --aimesh keep  --terrain keep
+
+    :: swy: delete the whole temporary folder to make sure it can be recreated and it's empty for any following files that need
+    ::      to be extracted afterwards, we don't want leftovers in there
     rmdir _tmp_unpack_folder /S /Q
     echo.
 )

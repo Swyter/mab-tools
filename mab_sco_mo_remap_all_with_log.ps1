169d9ac3c1ac25346d8dc3c16cfeed31c9098054
Swyter <swyterzone@gmail.com>
2023-07-27 16:51:48 +0200

    sco_mo_remap_all: Explain that the .ps1 file needs to be launched via right-clicking > 'Run with PowerShell'.

--- mab_sco_mo_remap_all_with_log.ps1
+++ mab_sco_mo_remap_all_with_log.ps1
@@ -1,2 +1,6 @@
-# swy: this launches the .cmd while also logging everything to a mab_sco_mo_remap_all.log file
+# swy: this launches the .cmd while also logging everything to a mab_sco_mo_remap_all.log file.
+#      run it via powershell; right-click this .ps1 file and click on 'Run with PowerShell'.
+
+#      PS: the log file will get overwritten every time you run this, so make sure to rename the file if
+#      you want to see the warnings, missing scene objects and other problems that may have been found.
 ./mab_sco_mo_remap_all.cmd 2>&1 | Tee-Object mab_sco_mo_remap_all.log

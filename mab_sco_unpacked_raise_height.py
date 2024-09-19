5e96a67b16fd001135e383892d59ef24d4019d9c
Swyter <swyterzone@gmail.com>
2024-09-20 01:34:45 +0200

    sco_unpacked_raise_height: Port over a reduced version of the PFM reader code from the sco_repack file. Add it to the GitHub builbot so that it gets included in the resulting .zip file.

--- mab_sco_unpacked_raise_height.py
+++ mab_sco_unpacked_raise_height.py
@@ -3,6 +3,7 @@ from struct import *
 import json, os
 import sys; from sys import exit
 import argparse
+import io
 
 def sco_unpacked_raise_height(input_folder, height):
     if not os.path.isdir(input_folder):
@@ -85,6 +86,92 @@ def sco_unpacked_raise_height(input_folder, height):
         print(f"[!] skipping AI mesh: {e}", file=sys.stderr)
 
     # swy: /part c/ elevate the heightmap vertices/pixels in the NetPBM portable float map (PFM) file
+    ground = {}
+    ground_layer = 'ground_elevation.pfm'
+    layer_name = ground_layer.split('.')[0].lower()
+    ext        = ground_layer.split('.')[1].lower()
+    last_scene_width = None; last_scene_height = None
+    try:
+        with open(f"{input_folder}/layer_{ground_layer}", 'rb') as f_image:
+            ascii_header = [] # swy: grab the three ASCII lines that make out the header of these NetPBM formats; strip their carriage returns and split each line into words/tokens
+            ascii_header_flattened = []
+            while True:
+                try:
+                    line = f_image.readline().decode('utf-8').replace('\n', '').replace('\r', '').replace('\t', ' ').strip()
+                    header_end_byte_offset = f_image.tell()
+                except UnicodeDecodeError:
+                    line = '' # swy: skip weird stuff or characters with wrong encoding
+                    break
+
+                # swy: ignore dummy second (comment) lines like the funky '# Created by GIMP version 2.10.32 PNM plug-in'
+                if not line or line[0] == '#' or line.startswith('--') or line[0] not in '0123456789+-.,P':
+                    continue
+
+                ascii_header += [line.split(' ')]
+                ascii_header_flattened = [value.strip() for sub_list in ascii_header for value in sub_list if value != ''] # swy: photoshop exports width and height in different lines, and maxval in the fourth line, don't depend on the line position, only the 'token' order matters
+
+                # swy: the format only has four tokens, that can be part of different lines or just separated by whitespace; this format is way too lax
+                if len(ascii_header_flattened) >= 4:
+                    break
+
+            if ascii_header_flattened[0][0] != 'P':
+                print(f'[e] invalid {ground_layer} header format; skipping')
+                exit(2)
+
+            magic  =       ascii_header_flattened[0]  # line 1, magic value
+            width  =   int(ascii_header_flattened[1]) # line 2, width and height
+            height =   int(ascii_header_flattened[2])
+            maxval = float(ascii_header_flattened[3]) # line 3, extra thing
+
+            # swy: grab how far away we are from the start after reading the ASCII part, use it to compute how many bytes the rest of the data takes
+            f_image.seek(0, io.SEEK_END); bytes_remain = f_image.tell() - header_end_byte_offset; f_image.seek(header_end_byte_offset)
+
+            # swy: actually read and interpret the binary data after the header, depending on the format/sub-variant
+            print(f'[i] found layer_{ground_layer}; type {magic}, {width} x {height}')
+
+            if magic == 'Pf' and ext == 'pfm':
+                assert maxval in [-1.0, 1.0]
+                cells_to_read = width * height
+                bytes_to_read = cells_to_read * 4; assert bytes_to_read == bytes_remain
+                contents_orig = []; contents_orig = unpack(f'{maxval < 0.0 and "<" or ">"}{cells_to_read}f', f_image.read(bytes_to_read)) # swy: handle both big (1.0, '>f') and little-endian (-1.0, '<f') floats; just in case
+
+                # swy: reverse the row ordering because the PFM format is from bottom to top, unlike every other NetPBM one, of course :)
+                for i in reversed(range(last_scene_height)):
+                    contents += contents_orig[i*last_scene_width : (i*last_scene_width) + last_scene_width][::-1] # swy: grab one "line" worth of data from the farthest point, and add it first; sort them backwards
+                
+                ground[layer_name] = contents
+            else:
+                print(f'[e] Unknown NetPBM format, {magic}: use Pf.'); exit(1)
+
+            exit(0)
+            # swy: write it
+            scene_width   = last_scene_width
+            scene_height  = last_scene_height
+            output_folder = input_folder
+            layer_str     = layer_name
+            try:
+                with open(f"{output_folder}/layer_{layer_str}.pfm", mode='wb') as fw:
+                    # swy: format spec at http://netpbm.sourceforge.net/doc/pfm.html; scanlines from left to right, from BOTTOM to top
+                    #      small three-line ASCII header with binary floats afterwards. e.g.: 
+                    #      Pf
+                    #      71 71
+                    #      -1.000
+                    fw.write(f'Pf\n{scene_width} {scene_height}\n-1.000\n'.encode('utf-8')) # swy: PF has RGB color, Pf is grayscale. align the first binary bytes to 16 bytes with the extra padded .000 in the ASCII part, make the number negative to let the program know that we're using little-endian floats
+
+                    flattened_list = [value for sub_list in ground[layer_str] for value in sub_list]
+                    reversed_list = []
+
+                    # swy: reverse the row ordering because the PFM format is from bottom to top, unlike every other NetPBM one, of course :)
+                    for i in reversed(range(scene_height)):
+                        reversed_list += flattened_list[i*scene_width : (i*scene_width) + scene_width][::-1] # swy: grab one "line" worth of data from the farthest point, and add it first; sort them backwards
+
+                    fw.write(pack(f'<{scene_width * scene_height}f', *reversed_list))
+            except OSError as e:
+                print(f"[e] couldn't open the pfm file: {e}", file=sys.stderr)
+
+
+    except FileNotFoundError:
+        print(f'[!]    no layer_{ground_layer} here, skipping...')
 
 
 
